b36e04cd22858d81f8958728434c331386881822
Nikita Goncharov <nikitagoncarov657@gmail.com>
2025-10-02 11:25:53 +0300

    ability to display of fields from related models

--- dynamic_list_display/admin.py
+++ dynamic_list_display/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.db import models
 from django.utils.html import format_html
-
+from django.core.exceptions import FieldDoesNotExist
 
 class DynamicFieldsModelAdmin(admin.ModelAdmin):
     default_fields = [
@@ -12,20 +13,58 @@ class DynamicFieldsModelAdmin(admin.ModelAdmin):
     def dynamic_fields_session_name(self):
         return f"{self.opts.app_label}.{self.__class__.__name__}"
 
+    def _model_field_exists(self, field: str) -> bool:
+        complex_field_parts = field.split("__")
+        if len(complex_field_parts) == 1:
+            try:
+                self.model._meta.get_field(field)
+                return True
+            except FieldDoesNotExist:
+                return False
+        else:
+            related_model = None
+            for index, field_part in enumerate(complex_field_parts):
+                if index < len(complex_field_parts)-1:
+                    # related model
+                    try:
+                        if related_model is None:
+                            related_model = self.model._meta.get_field(field_part).related_model
+                        else:
+                            related_model = related_model._meta.get_field(field_part).related_model
+                    except FieldDoesNotExist:
+                        return False
+                else:
+                    # last part is normal field
+                    try:
+                        related_model._meta.get_field(field_part)
+                        return True
+                    except FieldDoesNotExist:
+                        return False
+
     def get_list_display(self, request):
         if request.GET.getlist('fields'):
             request.session[self.dynamic_fields_session_name] = request.GET.getlist('fields')
 
         if request.session.get(self.dynamic_fields_session_name):
             selected_fields = request.session.get(self.dynamic_fields_session_name)
-            valid_fields = [f.name for f in self.model._meta.fields]
-            return [f for f in selected_fields if f in valid_fields]
+            return [field for field in selected_fields if self._model_field_exists(field)]
+
         return self.default_fields
 
     def changelist_view(self, request, extra_context=None):
+        if request.method != "GET":  # if action used, not just regular page load
+            return super().changelist_view(request, extra_context)
+        
         extra_context = extra_context or {}
 
-        all_fields = [f.name for f in self.model._meta.fields]
+        all_fields = []
+        for f in self.model._meta.fields:
+            if isinstance(f, models.OneToOneField) or isinstance(f, models.ForeignKey) or isinstance(f, models.ManyToManyField):
+                for related_field in f.related_model._meta.fields:
+                    all_fields.append(f"{f.name}__{related_field.name}")
+            else:
+                all_fields.append(f.name)
+
         selected_fields = request.session.get(self.dynamic_fields_session_name) or self.default_fields
 
         checkbox_html = ''.join([
